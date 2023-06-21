4ee834a26cddf507856dda5140d1bb9a5bbbc65d
haliljon <juraboevhaliljon@gmail.com>
2023-06-21 12:48:07 +0500

    Add some styling

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,7 +2,7 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const GoalItem = (props) => {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+        <Pressable onPress={props.onDeleteItem.bind(this, props.id)} style={({ pressed }) => pressed && styles.pressedItem}>
             <View style={styles.goalItem}>
                 <Text style={styles.goalText}>{props.text}</Text>
             </View>
@@ -15,11 +15,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
     },
     goalText: {
+        padding: 8,
         color: 'white'
+    },
+    pressedItem: {
+        opacity: .5
     }
 })
